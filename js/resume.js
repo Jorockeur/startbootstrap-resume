78d5384c9ec281b2d9bcfa3e289833637fc6f164
jorockeur <lacassagnev@gmail.com>
2019-04-15 13:08:42 +0200

    js scale animations, refactor gradient color algo

--- js/resume.js
+++ js/resume.js
@@ -27,58 +27,78 @@
 
   //custom rainbow colors javascript
   const body = document.getElementById('page-top');
-  const elements = [];
-  const socialNetworks = [];
+  const gradientCollection = document.getElementsByClassName('gradient');
+  const gradientBackgroundCollection = document.getElementsByClassName('gradient-bg');
+  const animationScaleCollection = document.getElementsByClassName('scale-animation');
   const hslHOffset = 110;
   const hslSOffset = 70;
   const hslLOffset = 42;
+  let gradientColor = `hsl(${hslHOffset}, ${hslSOffset}%, ${hslLOffset}%)`;
 
-  const elementsCollection = document.getElementsByClassName('gradient');
-  for (let element of elementsCollection) elements.push(element);
-
-  const socialNetworksCollection = document.getElementsByClassName('gradient-bg');
-  for (let socialNetwork of socialNetworksCollection) socialNetworks.push(socialNetwork);
-
-  for (let element of elements) {
-    if (element.tagName === 'NAV' || element.tagName === 'BUTTON') {
-      element.style.backgroundColor = `hsl(${hslHOffset}, ${hslSOffset}%, ${hslLOffset}%)`;
-      continue;
-    }
-    element.style.color = `hsl(${hslHOffset}, ${hslSOffset}%, ${hslLOffset}%)`;
-  }
-
-  for (let socialNetwork of socialNetworks) socialNetwork.style.color = `hsl(210, 9%, 31%)`;
-
-  const setColour = (base, elements, scrollTop, windowHeight) => {
+  const setGradientColor = (base, elements, scrollTop, windowHeight) => {
     let ratio = ((360 / base.clientHeight * (scrollTop + (scrollTop / base.clientHeight * windowHeight))) + hslHOffset);
-    let finalColour = `hsl(${ratio}, ${hslSOffset}%, ${hslLOffset}%)`;
-    for (let element of elements) {
-      if (element.tagName === 'NAV' || element.tagName === 'BUTTON') {
-        element.style.backgroundColor = finalColour;
-        continue;
+    gradientColor = `hsl(${ratio}, ${hslSOffset}%, ${hslLOffset}%)`;
+
+    if (elements === gradientCollection) {
+      for (let element of elements) {
+        element.style.color = gradientColor;
       }
-      element.style.color = finalColour;
+      return;
     }
-    for (let socialNetwork of socialNetworks) {
-      socialNetwork.style.color = finalColour;
+
+    for (let element of elements) {
+      element.style.backgroundColor = gradientColor;
     }
+  };
+
+  const setScale = (element, set) => {
+    if (set) {
+      element.animate([
+        { transform: 'scale(1)' },
+        { transform: 'scale(1.1)' },
+      ], {
+        duration: 200,
+        easing: 'ease-in-out'
+      });
+      element.style.transform = `scale(1.1)`;
+      return;
+    }
+    element.animate([
+      { transform: 'scale(1.1)' },
+      { transform: 'scale(1)' },
+    ], {
+      duration: 200,
+      easing: 'ease-in-out'
+    });
+    element.style.transform = `scale(1)`;
   };
 
   window.addEventListener('DOMContentLoaded', e => {
     const windowHeight = e.target.scrollingElement.clientHeight;
     const scrollTop = e.target.scrollingElement.scrollTop;
-    setColour(body, elements, scrollTop, windowHeight);
-  });
-
-  window.addEventListener('DOMContentLoaded', e => {
-    const windowHeight = e.target.scrollingElement.clientHeight;
-    const scrollTop = e.target.scrollingElement.scrollTop;
-    setColour(body, socialNetworks, scrollTop, windowHeight);
+    setGradientColor(body, gradientCollection, scrollTop, windowHeight);
   });
 
   window.addEventListener('scroll', e => {
     const windowHeight = e.target.scrollingElement.clientHeight;
     const scrollTop = e.target.scrollingElement.scrollTop;
-    setColour(body, elements, scrollTop, windowHeight);
+    setGradientColor(body, gradientCollection, scrollTop, windowHeight);
   });
+
+  window.addEventListener('DOMContentLoaded', e => {
+    const windowHeight = e.target.scrollingElement.clientHeight;
+    const scrollTop = e.target.scrollingElement.scrollTop;
+    setGradientColor(body, gradientBackgroundCollection, scrollTop, windowHeight);
+  });
+
+  window.addEventListener('scroll', e => {
+    const windowHeight = e.target.scrollingElement.clientHeight;
+    const scrollTop = e.target.scrollingElement.scrollTop;
+    setGradientColor(body, gradientBackgroundCollection, scrollTop, windowHeight);
+  });
+
+  for (let element of animationScaleCollection) {
+    element.addEventListener('mouseover', () => setScale(element, true));
+    element.addEventListener('mouseout', () => setScale(element, false));
+  }
 })(jQuery); // End of use strict
